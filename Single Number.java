3b922d7277ab0c0e1198060ae7a82f259d0404e6
Vishal Dhanjani <vdhanjani63@gmail.com>
2024-02-26 19:43:13 +0530

    Update Single Number.java

--- Single Number.java
+++ Single Number.java
@@ -19,3 +19,25 @@ class Solution {
         return -1;
     }
 }
+
+
+/*
+Optimal solution
+Two important properties of XOR are the following:
+XOR of two same numbers is always 0 i.e. a ^ a = 0. ←Property 1.
+XOR of a number with 0 will result in the number itself i.e. 0 ^ a = a.  ←Property 2
+Here all the numbers except the single number appear twice and so will form a pair. 
+Now, if we perform XOR of all elements of the array, the XOR of each pair will result in 0 according to the XOR property 1.
+The result will be = 0 ^ (single number) = single number (according to property 2).
+Time complexity - O(N)
+Space complexity - O(1)
+*/
+class Solution {
+    public int singleNumber(int[] nums) {
+        int xor = 0;
+        for(int num: nums){
+            xor=xor^num;
+        }
+        return xor;
+    }
+}
